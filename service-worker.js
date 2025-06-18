0f8e9f4c21d94ca7fb4d9a3de409f3e1a0225804
Xavier <81702706+XavierSOTP@users.noreply.github.com>
2025-06-18 11:16:24 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,20 @@
+const CACHE_NAME = "quickcam-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "manifest.json",
+  "icon.png"
+];
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("quickcam").then(cache => cache.addAll(["index.html", "manifest.json", "icon.png"]))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
+
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
   );
 });
